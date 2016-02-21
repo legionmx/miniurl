50bb0bdd794f13fac3783569e19d68576b11bd6d
legionmx <fimcwan+github@gmail.com>
2016-02-20 21:38:43 -0600

    implemented default function parameters workaround
    In the master, default function parameters are used. Chrome 48 does not
    comply with ECMAScript15. This commit is supposed to put a workaround in
    place.

--- miniurl/index.js
+++ miniurl/index.js
@@ -20,7 +20,12 @@ $(document).ready(function(){
 		
 	};
 
-	var cambiarUIpostHash = function(error = "", color = "green", sePuedeGenerar = true, hashgen = ""){
+	//var cambiarUIpostHash = function(error = "", color = "green", sePuedeGenerar = true, hashgen = ""){
+	var cambiarUIpostHash = function(error, color, sePuedeGenerar, hashgen){
+		var error = error || "";
+		var color = color || "green";
+		var sePuedeGenerar = sePuedeGenerar || true;
+		var hashgen = hashgen || "";
 		$("#error").html(error);
 		$("#error").css('color',color);
 		$("#generar").prop('disabled',!sePuedeGenerar);
