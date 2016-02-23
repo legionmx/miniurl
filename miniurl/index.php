fc2879108ab59d2d9c7b86a2213d2b797f489ac7
legionmx <fimcwan+github@gmail.com>
2016-02-23 13:44:05 -0600

    migration of main index.php to Bootstrap
    The main index was rewritten to use bootstrap for a better view. This is
    a first approach to a full bootstrap use. There are still things to
    implement with it.

--- miniurl/index.php
+++ miniurl/index.php
@@ -1,26 +1,56 @@
 <?php
 	include("const.php");
 ?>
+<!DOCTYPE html>
 <html>
 <head>
-<title>MINIURL</title>
-<!--<script type="text/javascript" src="jquery-2.2.0.js"></script>-->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-<script type="text/javascript" src="index.js"></script>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>MINIURL</title>
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 </head>
 <body>
-	<h1>M I N I U R L</h1>
-	<br>
-	<select id="protocolo" name="protocolo">
-		<?php
-		foreach (CONS::PROTOCOLOS as  $cveProt => $abvProt) {
-			echo "<option value='$cveProt'>$abvProt</option>\n";
-		}
-		?>
-	</select>
-	<input type="text" size=100 name="url" id="url">
-	<input type="button" id="generar" value="Generar" disabled>
-	<p id="error"></p>
-	<p><em><?php echo CONS::BASEURL;?><span id="hashgen"></span></em><br><label>¿Logear visitas?</label><input type="checkbox" id="conLog"></p>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12"><h2>M I N I U R L</h2></div>
+		</div>
+		<div class="row">
+			<div class="col-md-2">
+				<select id="protocolo" name="protocolo" class="form-control">
+					<?php
+					foreach (CONS::PROTOCOLOS as  $cveProt => $abvProt) {
+						echo "<option value='$cveProt'>$abvProt</option>\n";
+					}
+					?>
+				</select>
+			</div>
+			<div class="col-md-4">
+				<input type="url" name="url" id="url" class="form-control" placeholder="Direcci&oacute;n a minimizar">
+			</div>
+			<div class="col-md-1">
+				<!-- <input type="button" id="generar" class="btn btn-default" value="Generar" disabled>-->
+				<button type="submit" id="generar" class="btn btn-default" disabled="">Generar</button>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<p id="error"></p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<p><em><?php echo CONS::BASEURL;?><span id="hashgen"></span></em><br><label for="conLog">¿Logear visitas?</label>&nbsp;<input type="checkbox" id="conLog"></p>
+			</div>
+		</div>
+	</div>
+
+	<!-- JQuery y Bootstrap-->
+	<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	<!-- Scripts propios -->
+	<script type="text/javascript" src="index.js"></script>
 </body>
 </html>
