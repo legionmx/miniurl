4c0ff67957fc4646ea78f0e6abffd3d249be9711
legionmx <fimcwan+github@gmail.com>
2016-02-23 16:22:39 -0600

    First alias implementation commit
    The 1st alias implementation commit. It changes the main UI a little
    bit, and adds a WS that checks if the alias provided exists. BD lookups
    are not actually implemented yet.

--- miniurl/index.js
+++ miniurl/index.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
 	var generarHash = function(){
 		var prot = $("#protocolo").val();
 		var uerele = $("#url").val();
-		//if($("#conLog").prop('checked')) alert("Se logeara");
+		//if($("#conLog").prop('checked')) alert("Se logeara --"+ $("#conLog").prop('checked'));
+		//else alert("NO se logeara --"+ $("#conLog").prop('checked'));
 		if(uerele.length<8){
 			cambiarUIpostHash("Debe ser una direcci&oacute; de al menos 8 caracteres",'orange',false);
 		}
@@ -28,6 +29,40 @@ $(document).ready(function(){
 		$("#hashgen").css('color',color);
 	};
 
+	var salvarAlias = function(){
+		//Obtenemos valores a enviar
+		var prot = $("#protocolo").val();
+		var uerele = $("#url").val();
+		var alias = $("#alias").val();
+
+		//Validamos la entrada
+		if(alias.length<3){
+			//El alias es muy muy corto
+			//alert("Alias muy muy corto");
+			$("#error").html("Por lo menos 3 caracteres");
+			$("#error").css('color','orange');
+			$("#salvar").prop('disabled',true);
+			//$("#salvar").removeClass();
+		}
+		else{
+			//alert("El alias es de buen tamaño. Se comprobara existencia en BD");
+			$("#error").html("");
+			$("#error").css('color','black');
+			$("#salvar").prop('disabled',false);
+
+			$.getJSON("chkAlias.php", {'alias': alias}, function(response){
+				if(response.existe){
+					//alert("El alias existe");
+					$("#alias-group").addClass('has-error');
+				}
+				else{
+					//alert("El alias NO existe");
+					$("#alias-group").addClass('has-success');
+				}
+			});
+		}
+	};
+
 	$("#generar").click(function(){
 		//Insertamos el link minimizado
 		var prot = $("#protocolo").val();
@@ -39,12 +74,14 @@ $(document).ready(function(){
 		});
 	});
 	$("#url").on("input",function(evento){
-		//alert("Statgefunden Input");
 		generarHash();
 	});
 	$("#protocolo").on("change",function(evento){
-		//alert("Statgefunden Select Change");
 		generarHash();
+	});
+
+	$("#alias").on("input",function(evento){
+		salvarAlias();
 	});
 });
 
