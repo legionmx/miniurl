79e59223e46716b3b14987f2a73254fd4e4a0072
legionmx <fimcwan+github@gmail.com>
2016-03-03 12:30:46 -0600

    First commit of the users feat implementation
    A lot has been done. This commit includes changes to several views, the
    new login view, minor changes in controllers, and the addition of a more
    formal Model structure under the class directory.
    This commit also includes changes relating to the use of absolute paths,
    and the use of a lingua franca

--- miniurl/index.php
+++ miniurl/index.php
@@ -1,5 +1,6 @@
 <?php
 	require_once("const.php");
+	session_start();
 ?>
 <!DOCTYPE html>
 <html>
@@ -7,7 +8,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
  	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>MINIURL</title>
+	<title>MINIURL - Home</title>
 
 	<!-- CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
@@ -21,14 +22,23 @@
 			</div>
 			<div id="navbar">
 				<ul class="nav navbar-nav navbar-right">
-					<li class="active"><a href="#">Inicio</a></li>
-					<li><a href="stats/">Estad&iacute;sticas</a></li>
+					<li class="active"><a href="#">Home</a></li>
+					<?php if(isset($_SESSION['authToken'])){ ?>
+					<li><a href="/stats/">Statistics</a></li>
+					<li><a href="/auth/logout.php">Logout</a></li>
+					<?php
+					}
+					else{
+					?>
+					<li><a href="/auth/">Login</a></li>
+					<?php } ?>
 				</ul>
 			</div>
 		</div>
 	</nav>
+	
 	<div class="container">
-		<div class="row">
+		<div class="row page-header">
 			<div class="col-md-12"><h2>M I N I U R L</h2></div>
 		</div>
 
@@ -36,7 +46,6 @@
 			<div class="col-md-2">
 				<select id="protocolo" name="protocolo" class="form-control">
 					<?php
-					//foreach (CONS::PROTOCOLOS as  $cveProt => $abvProt) {
 					foreach($_PROTOCOLOS as $cveProt => $abvProt) {
 						echo "<option value='$cveProt'>$abvProt</option>\n";
 					}
@@ -44,17 +53,11 @@
 				</select>
 				<input type="text" id="prot_propio" class="form-control input-sm hidden" placeholder="Protocolo">
 			</div>
-			<!-- <div class="col-md-2">
-				<input type="text" id="prot_propio" class="form-control hidden" placeholder="Protocolo">
-			</div> -->
-			<!-- <div class="col-md-2 hidden" id="div_prot_propio">
-				<input type="text" id="prot_propio">
-			</div> -->
 			<div class="col-md-4">
-				<input type="url" name="url" id="url" class="form-control" placeholder="Direcci&oacute;n a minimizar">
+				<input type="url" name="url" id="url" class="form-control" placeholder="URL to minimize">
 			</div>
 			<div class="col-md-1">
-				<button type="submit" id="generar" class="btn btn-default" disabled>Generar Alias</button>
+				<button type="submit" id="generar" class="btn btn-default" disabled>Generate Alias</button>
 			</div>
 		</div>
 
@@ -69,25 +72,24 @@
 			</div>
 		</div>
 
-		
-
-
 		<div class="row">
 			<div class="col-md-12">
 				<p id="error"></p>
 			</div>
 		</div>
+		<?php if(isset($_SESSION['authToken'])) { ?>
 		<div class="row">
 			<div class="col-md-12">
 				<p>
-					<label for="conLog">¿Logear visitas?</label>&nbsp;<input type="checkbox" id="conLog">
+					<label for="conLog">Log visits?</label>&nbsp;<input type="checkbox" id="conLog">
 				</p>
 			</div>
 		</div>
+		<?php } ?>
 		<div class="row">
 			<div class="col-md-12">
 				<p>
-					<button type=submit id="salvar" class="btn btn-primary" disabled>Salvar url minimizado</button>
+					<button type=submit id="salvar" class="btn btn-primary" disabled>Save cool URL</button>
 				</p>
 			</div>
 		</div>
