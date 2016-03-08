69e5e1f808a89875fb4218a0112147ea04c57ac1
legionmx <fimcwan+github@gmail.com>
2016-03-08 15:17:12 -0600

    adding standard look and feel to remaining views
    The standard look and feel was added to main index, the /auth views and
    the /stats views. It includes the logo.

--- miniurl/auth/index.php
+++ miniurl/auth/index.php
@@ -15,13 +15,16 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/const.php');
 	<!-- CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 	<link href="/css/sticky-footer.css" rel="stylesheet">
+	<link href="/css/custom.css" rel="stylesheet">
+	<link rel="stylesheet" href="/css/owl.carousel.css"/>
+    <link rel="stylesheet" href="/css/owl.theme.css"/>
 </head>
 <body>
 
 	<nav class="navbar navbar-fixed-top navbar-inverse">
 		<div class="container">
 			<div class="navbar-header">
-				<a class="navbar-brand">M I N I U R L</a>
+				<a class="navbar-brand"><img src="/images/logo_white.png" height="73" width="230" /></a>
 			</div>
 			<div id="navbar">
 				<ul class="nav navbar-nav navbar-right">
@@ -33,10 +36,49 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/const.php');
 		</div>
 	</nav>
 
+	<!-- Slider start -->
+    <section id="slider_part">
+         <div class="carousel slide" id="carousel-example-generic" data-ride="carousel">
+            <!-- Indicators -->
+         	 <ol class="carousel-indicators text-center">
+                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+             </ol>
+
+           	<div class="carousel-inner">
+           	 	<div class="item active">
+           	 		<div class="overlay-slide">
+           	 			<img src="/images/p1.jpg" alt="" class="img-responsive">
+           	 		</div>
+           	 	</div>
+                <div class="item">
+                    <div class="overlay-slide">
+                        <img src="/images/p2.jpg" alt="" class="img-responsive">
+           	 		</div>
+           	 	</div>
+
+           	 </div> 	 <!-- End Carousel Inner -->
+
+            <!-- Controls -->
+            <div class="slides-control ">
+                <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                	<span><i class="fa fa-angle-left"></i></span>
+                </a>
+                <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                	<span><i class="fa fa-angle-right"></i></span>
+                </a>
+            </div>
+        </div>
+  	</section>
+    <!--/ Slider end -->
+
+    <div class="carousel-caption">
+
 	<div class="content">
 		<div class="row page-header">
 			<div class="col-md-8 col-md-offset-2">
-				<h1>Login</h1>
+				<h2>Login</h2>
 			</div>
 		</div>
 		<div class="col-md-8 col-md-offset-2 form-inline">
@@ -54,10 +96,15 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/const.php');
 		</div>
 	</div>
 
+	</div>
+
 	<!-- JQuery y Bootstrap-->
 	<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 	<!-- Own scripts -->
 	<script type="text/javascript" src="indexAuth.js"></script>
+	<!-- Other scripts-->
+	<script type="text/javascript" src="/js/jquery.bxslider.min.js"></script>
+	<script src="/js/owl.carousel.min.js"></script>
 </body>
 </html>
--- miniurl/stats/index.php
+++ miniurl/stats/index.php
@@ -19,12 +19,16 @@ $uid = $_SESSION['uid'];
 
 	<!-- CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	<link href="css/sticky-footer.css" rel="stylesheet">
+	<link href="/css/custom.css" rel="stylesheet">
+	<link rel="stylesheet" href="/css/owl.carousel.css"/>
+    <link rel="stylesheet" href="/css/owl.theme.css"/>
 </head>
 <body>
 	<nav class="navbar navbar-fixed-top navbar-inverse">
 		<div class="container">
 			<div class="navbar-header">
-				<a class="navbar-brand">M I N I U R L</a>
+				<a class="navbar-brand"><img src="/images/logo_white.png" height="73" width="230" /></a>
 			</div>
 			<div id="navbar">
 				<ul class="nav navbar-nav navbar-right">
@@ -44,6 +48,45 @@ $uid = $_SESSION['uid'];
 			</div>
 		</div>
 	</nav>
+
+	<!-- Slider start -->
+    <section id="slider_part">
+         <div class="carousel slide" id="carousel-example-generic" data-ride="carousel">
+            <!-- Indicators -->
+         	 <ol class="carousel-indicators text-center">
+                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+             </ol>
+
+           	<div class="carousel-inner">
+           	 	<div class="item active">
+           	 		<div class="overlay-slide">
+           	 			<img src="/images/p1.jpg" alt="" class="img-responsive">
+           	 		</div>
+           	 	</div>
+                <div class="item">
+                    <div class="overlay-slide">
+                        <img src="/images/p2.jpg" alt="" class="img-responsive">
+           	 		</div>
+           	 	</div>
+
+           	 </div> 	 <!-- End Carousel Inner -->
+
+            <!-- Controls -->
+            <div class="slides-control ">
+                <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                	<span><i class="fa fa-angle-left"></i></span>
+                </a>
+                <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                	<span><i class="fa fa-angle-right"></i></span>
+                </a>
+            </div>
+        </div>
+  	</section>
+    <!--/ Slider end -->
+
+    <div class="carousel-caption">
 
 	<div class="container">
 		<div class="row" class="page-header">
@@ -79,8 +122,13 @@ $uid = $_SESSION['uid'];
 		</div>
 	</div>
 
+	</div>
+
 	<!-- JQuery y Bootstrap-->
 	<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	<!-- Other scripts -->
+	<script type="text/javascript" src="/js/jquery.bxslider.min.js"></script>
+	<script src="/js/owl.carousel.min.js"></script>
 </body>
 </html>
