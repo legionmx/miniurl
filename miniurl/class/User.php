fa62ce6282182811d0eac6f4675d33b4acf5b435
legionmx <fimcwan+github@gmail.com>
2016-03-08 09:58:06 -0600

    fixing merge conflicts
    Fixing merge congflicts in the User class, after @pikosmanterola merge.
    Most were cleanup edits that were undone. Nothing major.

--- miniurl/class/User.php
+++ miniurl/class/User.php
@@ -4,15 +4,10 @@
 */
 //namespace miniurl;
 require_once($_SERVER['DOCUMENT_ROOT'].'/const.php');
-<<<<<<< HEAD
-//The next var contains the fields that are persisted in the DB
-$USER_FIELDS = array('username','firstName','lastName','dob','email','password');
-=======
 
 //The next var contains the fields that are persisted in the DB
 $USER_FIELDS = array('username','firstName','lastName','dob','email','password');
 
->>>>>>> 59312cc072c4f08aef08dc392ac1fb842db502d4
 class User
 {
 	protected $id;
@@ -22,35 +17,21 @@ class User
 	protected $lastName;
 	protected $dob; //Date of Birth, fecha de nacimiento
 	protected $email;
-<<<<<<< HEAD
 	protected $role; //El id por el momento, pero deber’a ser otro objeto
 	protected $active;
 	protected $registered;
-=======
-	protected $role; //El id por el momento, pero deberÃ­a ser otro objeto
-	protected $active;
-	protected $registered;
 
->>>>>>> 59312cc072c4f08aef08dc392ac1fb842db502d4
 	function __construct($username)
 	{
 		$this->registered = false; //We assume the user is not registered yet
 		$this->active = false; //Until registered, we cannnot truly say it is active
 		$this->password = null;
-<<<<<<< HEAD
-=======
-
->>>>>>> 59312cc072c4f08aef08dc392ac1fb842db502d4
 		//We search the user in the DB to load it
 		global $base;
 		$sqlUser = "select * from users where username='$username'";
 		$rsUser = $base->Execute($sqlUser);
 		if($rsUser->RecordCount() == 0){ //The user is not persisted in DB
 			//throw new Exception("The user doesn't exist in the DB", 1);
-<<<<<<< HEAD
-=======
-
->>>>>>> 59312cc072c4f08aef08dc392ac1fb842db502d4
 		}
 		else{
 			$data = $rsUser->fetchNextObject(false);
@@ -68,7 +49,7 @@ class User
 			if($data->active != '0') $this->active = true;
 		}
 	}
-<<<<<<< HEAD
+
 	function __toString(){
 		return $this->username.' --- '.$this->firstName.' '.$this->lastName;
 	}
@@ -79,31 +60,12 @@ class User
 	public function isRegistered(){
 		return $this->registered;
 	}
-=======
 
-	function __toString(){
-		return $this->username.' --- '.$this->firstName.' '.$this->lastName;
-	}
-
-	/*** Getters/Setters ***/
-
-	public function getId(){
-		return $this->id;
-	}
-
-	public function isRegistered(){
-		return $this->registered;
-	}
-
->>>>>>> 59312cc072c4f08aef08dc392ac1fb842db502d4
 	public function matchPassword($givenPassword){
 		$hashedPassword = hash('sha256', $givenPassword);
 		return $hashedPassword === $this->password;
 	}
-<<<<<<< HEAD
-=======
 
->>>>>>> 59312cc072c4f08aef08dc392ac1fb842db502d4
 	public static function create($data){
 		global $USER_FIELDS;
 		global $base;
@@ -113,10 +75,7 @@ class User
 		if(!isset($data['username'])){
 			throw new Exception("A username is needed", 1);
 		}
-<<<<<<< HEAD
-=======
 
->>>>>>> 59312cc072c4f08aef08dc392ac1fb842db502d4
 		try{
 			$registeredUser = new User($data['username']);
 		} catch (Exception $e){
@@ -124,10 +83,7 @@ class User
 		}
 		//if($registeredUser instanceof User) throw new Exception("The username is already registered", 1);
 		if($registeredUser->isRegistered()) throw new Exception("The username is already registered", 1);
-<<<<<<< HEAD
-=======
 
->>>>>>> 59312cc072c4f08aef08dc392ac1fb842db502d4
 		//We prepare the insert
 		$insFields = "";
 		$insValues = "";
@@ -135,18 +91,12 @@ class User
 			if(in_array($field, $USER_FIELDS)){
 				if(!empty($insValues)) $insValues.=',';
 				if(!empty($insFields)) $insFields.=',';
-<<<<<<< HEAD
-=======
 
->>>>>>> 59312cc072c4f08aef08dc392ac1fb842db502d4
 				//Special handling of password
 				if($field=='password'){
 					$value = hash('sha256', $value);
 				}
-<<<<<<< HEAD
-=======
 
->>>>>>> 59312cc072c4f08aef08dc392ac1fb842db502d4
 				$insFields.="$field";
 				if(is_numeric($value)){
 					$insValues.="$value";
@@ -160,13 +110,9 @@ class User
 			}
 		}
 		$sqlInsUser = "insert into users($insFields) values ($insValues)";
-<<<<<<< HEAD
-		$rsInsUser = $base->Execute($sqlInsUser);
-=======
 
 		$rsInsUser = $base->Execute($sqlInsUser);
 
->>>>>>> 59312cc072c4f08aef08dc392ac1fb842db502d4
 		return new User($data['username']);
 	}
 }
