ead3c9f720bf85aad40eb88034c891c5bbc1e08d
legionmx <fimcwan+github@gmail.com>
2016-03-10 15:10:56 -0600

    clean up
    Clean up of the sign up JS script. This was supposed to be on the last
    commit. But I forgot to Ctrl-S .

--- miniurl/auth/signUp.js
+++ miniurl/auth/signUp.js
@@ -44,20 +44,6 @@ $(document).ready(function(){
 		$("#messageContent").removeClass('has-error has-warning');
 	};
 
-	var passwordsMatch = function(){
-		return $("#password").val() == $("#passcheck").val();
-	}
-
-	var isValidSignupForm = function(){
-		console.log("Using the validation function outside the Form object. This usage is deprecated");
-		return false;
-		/*var formToSend = new FormSubmit();
-		if(formToSend.email.length<=0) return false;
-		if(formToSend.password.length<=0) return false;
-		if(!passwordsMatch()) return false;
-		return true;*/
-	}
-
 	/* Event handlers */
 	$("#signup").click(function(){
 		var form = new FormSubmit();
@@ -66,30 +52,19 @@ $(document).ready(function(){
 			$.post('signup.php',{'email': form.email, 'password': form.password, 'firstName': form.firstName, 'lastName': form.lastName, 'ss': 'blah'},function(data,status){
 				//$("#messageContent").html(data.message);
 				if(data.status == '1'){
-					//$("#messageContent").html("<em class='help-block'>Succesful registration! Redirecting to main page</em>");
-					//$("#messageContent").addClass('has-success');
-					//$("#messages").removeClass('hidden');
 					changeMessage(data.message,'has-success');
 					window.setTimeout(function(){
 						window.location.replace('/auth/');
 					},5000);
 				}
 				else{
-					//TODO: Error display and related UI change
-					//$("#messageContent").addClass('has-warning');
 					changeMessage(data.message,'has-warning');
 				}
-				//$("#messages").removeClass('hidden');
 				//console.log(status);
 				console.log(data.log);
 			},'json');
 		}
 		else{
-			/*$("#messageContent").html("<em class='help-block'>The passwords don't match</em>");
-			$("#messageContent").addClass('has-error');
-			$("#messages").removeClass('hidden');*/
-			//changeMessage("<em class='help-block'>The passwords don't match</em>",'has-error')
-			//changeMessage("<em class='help-block'>The registration form has errors</em>",'has-error');
 			changeMessage("<em class='help-block'>"+form.errorMsg+"</em>",'has-error');
 
 			//console.log("Pass1: "+password);
